fe24b5d1088bcf41cdb46791d29bbe6e637044d1
Alfonso Rush <alfonso.rush@protonmail.com>
2015-10-06 17:20:32 -0700

    getting started

---
+++ js/tests/code_test.js
@@ -0,0 +1,9 @@
+QUnit.skip("As a user, I can see whether Free Code Camp is currently streaming on Twitch.tv");
+
+QUnit.skip("As a user, I can click the status output and be sent directly to the Free Code Camp's Twitch.tv channel");
+
+QUnit.skip("As a user, if Free Code Camp is streaming, I can see additional details about what they are streaming.");
+
+QUnit.skip("As a user, I can search through the streams listed.");
+
+QUnit.skip("As a user, I will see a placeholder notification if a streamer has closed their Twitch account. You can verify this works by adding brunofin and comster404 to your array of Twitch streamers.");

22a97b8ab00cd387b9328616b2862b52ef9fc336
Alfonso Rush <alfonso.rush@protonmail.com>
2015-10-07 21:09:04 -0700

    wip-writing test

--- js/src/codelib.js
+++ js/src/codelib.js
@@ -1,7 +1,33 @@
 "use strict";
 define(function() {
-  var code  = function(a, b) {
+  //constructor
+  function Codelib(a,b){
+    // if u had passed vars
+    this.a = a;
+    this.b = b;
+
+  }
+
+  //methods
+  Codelib.prototype.code = function(a, b) {
     return (a + b);
   };
-  return code;
+
+  //methods
+  Codelib.prototype.gotjson = function(){
+    var result = $.getJSON("https://api.twitch.tv/kraken/streams/MedryBW",function(json){
+      if (json.length==0){
+        console.log('error no json');
+        console.dir(json);
+        return json;
+      }else{
+        console.log('got json: ');
+        console.dir(json);
+        return json;
+      }
+    });
+    return result;
+  };
+
+  return Codelib;
 });
--- js/tests/codeTest.js
+++ js/tests/codeTest.js
@@ -1,10 +1,28 @@
 "use strict";
-define(['src/codelib'], function(codelib){
+define(['src/codelib','jquery'], function(Codelib){
   var run = function(){
-    test('codelib should return the sum of the two supplied numbers.',function(){
-      equal(codelib(1,1),2, 'The return should be 2.');
-      equal(codelib(-2,1),-1, 'The return should be -1.');
+    QUnit.test('code should return the sum of the two supplied numbers.',function(assert){
+      var codelib = new Codelib();
+      assert.equal(codelib.code(1,1),2, 'The return should be 2.');
+      assert.equal(codelib.code(-2,1),-1, 'The return should be -1.');
     });
+
+    QUnit.test("As a user, I can see whether Free Code Camp is currently streaming on Twitch.tv",function(assert){
+      var codelib = new Codelib();
+      console.log('in test: ' + codelib.gotjson());
+      assert.equal(codelib.gotjson().readyState,1, 'should be true');
+    });
+
+    QUnit.skip("As a user, I can click the status output and be sent directly to the Free Code Camp's Twitch.tv channel");
+
+    QUnit.skip("As a user, if Free Code Camp is streaming, I can see additional details about what they are streaming.");
+
+    QUnit.skip("As a user, I can search through the streams listed.");
+
+    QUnit.skip("As a user, I will see a placeholder notification if a streamer has closed their Twitch account. You can verify this works by adding brunofin and comster404 to your array of Twitch streamers.");
+
+
   };
   return {run: run}
 });
+
